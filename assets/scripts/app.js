776a75579477edecfdc6e8ea1e8fc8a815c18b2f
Luis Ramirez <sdaewa22@gmail.com>
2020-02-25 11:13:12 +0000

    smooth scrolling added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -117,6 +117,9 @@ class DOMHelper {
         const element = document.getElementById(elementId);
         const destinationElement = document.querySelector(newDestinationSelector);
         destinationElement.append(element);
+        element.scrollIntoView({
+            behavior: "smooth"
+        });
     }
 }
 class ProjectList {
